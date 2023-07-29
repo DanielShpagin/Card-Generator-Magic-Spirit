c289cd90fee3bc4edbe8404aeffd3b7972b3e44b
Daniel Shpagin <94217221+DanielShpagin@users.noreply.github.com>
2023-07-29 19:28:40 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -302,7 +302,7 @@
             if (lines.length === 3) lineY += -5 * scale;
             if (lines.length === 4) lineY -= 10 * scale;
 
-            if (card_type === 'spell') lineY -= 1 * scale;
+            if (card_type === 'spell') lineY += 1 * scale;
 
             for (var j = 0; j < parts.length; j++) {
                 if (parts[j] === '[b]') {
